0dfd44b39e3def9bc8fa72b64e081ae219127e46
ReFo <77904942+ReFoBey@users.noreply.github.com>
2021-07-30 16:26:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,64 +1,7 @@
-#include <tchar.h>
-#include <random>
-#pragma comment(lib, "urlmon.lib")
-#include <urlmon.h>
-#include <sstream>
-#include <iostream>
-#include <Lmcons.h>
-#include <string>
-#include <random>
-#include <Windows.h>
-#include "md5.h"
-#include <ctime>
-#include <string.h>
-#include <string>
-#include <stdio.h> 
-#include <fstream>
-#include <chrono>
-#pragma comment(lib, "user32.lib")
-using namespace std;
-
-using namespace std;
-
-string key, mixing, mixed;
-
-template< typename ... Args >
-std::string stringer(Args const& ... args)
-{
-    std::ostringstream stream;
-    using List = int[];
-    (void)List {
-        0, ((void)(stream << args), 0) ...
-    };
-    return stream.str();
-}
-
-string hwidcek() {
-    time_t now = time(0);
-    string time = to_string(now);
-    string finalTime = time.substr(0, 7);
-
-    string a, b, c, d, e;
-    HW_PROFILE_INFO hwProfileInfo;
-    GetCurrentHwProfile(&hwProfileInfo);
-    wstring hwidWString = hwProfileInfo.szHwProfileGuid;
-    string hwid(hwidWString.begin(), hwidWString.end());
-    
-    a = hwid.substr(1, 8);
-    b = hwid.substr(10, 4);
-    c = hwid.substr(15, 4);
-    d = hwid.substr(20, 4);
-    e = hwid.substr(25, 12);
-
-    string mix = stringer(a, c, d, e, a, b, e, a, d, c, e, a);
-    mixing = md5(md5(md5(md5(md5(mix)))));
-    string birlestir = stringer(mixing, finalTime, mixing);
-    mixed = md5(md5(birlestir));
-    return mixed;
-}
+#include "misc.h"
 
 void lisanskontrol() {
-    string url = "http://0.0.0.0/license.php?license=" + key + "&hwid=" + mixing;
+    string url = "http://0.0.0.0/license.php?license=" + anahtar + "&hwid=" + mixx;
     std::wstring url2(url.begin(), url.end());
     IStream* stream;
     HRESULT result = URLOpenBlockingStream(0, (LPCWSTR)url2.c_str(), &stream, 0, 0);
@@ -85,6 +28,6 @@ void lisanskontrol() {
 int main() {
     hwidcek();
     cout << "Lisans Anahtarı: ";
-    cin >> key;
+    cin >> anahtar;
     lisanskontrol();
 }
